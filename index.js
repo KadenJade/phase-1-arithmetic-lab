f71ec5a1387c6eb1461d2fb4f5a5773c4ab24fb8
Kaden <kadengraybill@gmail.com>
2022-08-29 12:45:42 -0500

    commit tree 3cdfa4f3f062b638edce8d6f1cb798da884aac05

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 6.2;
+let num2 = 10;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+// 3
+let mod;
+let whole;
+let remains;
+let num3 = 53;
+let num4 = 7
+whole = Math.floor(num3 / num4);
+mod = (num3) - (num4 * whole); 
+
+//4
+let max;
+max = Math.max(1, 12, 14, 15, 17, 20);
